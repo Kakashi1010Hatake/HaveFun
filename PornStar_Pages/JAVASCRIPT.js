11fe0a61578566bdae5d5e41cb8aa195bfb75852
Kakashi Hatake <kakashi1010hatake@gmail.com>
2025-04-03 02:28:51 +0200

    Add files via upload

---
+++ PornStar_Pages/JAVASCRIPT.js
@@ -0,0 +1,356 @@
+class SpecialHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <header>
+            <div class="logo">
+                <p onclick="OpenMenu()">Porn<span>Tube</span></p>
+            </div>
+            <div class="close_btn"><i class="material-icons" onclick="CloseMenu()">close</i></div>
+            <div class="playMe" onclick="PlayWithMe()"><img src="01.png"></div>
+            <section class="sidebar">
+                <div class="mine">
+                    <div class="log">
+                        <a href="../../index.html"><i class="material-icons">settings</i></a>
+                    </div>
+                    <div class="user">
+                        <div class="circle_one">
+                            <div class="circle_two">
+                                <div class="circle_third">
+                                    <p class="userPic">J</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="log">
+                        <a href="../../index.html"><i class="material-icons">exit_to_app</i></a>
+                    </div>
+                </div>
+                <nav>
+                    <ul class="ul">
+                        <li class="homy"><a href="../../Main.html">Home</a></li>
+                        <li><a href="../../Letters_Pages/Letters_Pages - Porn/A_Page.html">PornStar</a></li>
+                        <li><a href="../../Collections.html">Collections</a></li>
+                        <li><a href="../../Lesbian.html">Lesbian</a></li>
+                        <li><a href="../../Masturbation/01.html">Mastrbution</a></li>
+                        <li><a href="../../Movies.html">Movies</a></li>
+                        <li class="logOut"><a href="../../index.html">Log Out</a></li>
+                    </ul>
+                </nav>
+            </section>
+        </header>
+        `
+    }
+}
+
+customElements.define('special-header', SpecialHeader);
+
+//--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
+
+class SpecialHeader2 extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <header>
+            <div class="logo">
+                <p onclick="OpenMenu()">Porn<span>Tube</span></p>
+            </div>
+            <div class="close_btn"><i class="material-icons" onclick="CloseMenu()">close</i></div>
+            <section class="sidebar">
+                <div class="mine">
+                    <div class="log">
+                        <a href="../../index.html"><i class="material-icons">settings</i></a>
+                    </div>
+                    <div class="user">
+                        <div class="circle_one">
+                            <div class="circle_two">
+                                <div class="circle_third">
+                                    <p class="userPic">J</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="log">
+                        <a href="../../index.html"><i class="material-icons">exit_to_app</i></a>
+                    </div>
+                </div>
+                <nav>
+                    <ul class="ul">
+                        <li><a href="../Main.html">Home</a></li>
+                        <li><a href="../Letters_Pages/A_Page.html">PornStar</a></li>
+                        <li><a href="../Collections.html">Collections</a></li>
+                        <li><a href="../Lesbian/01.html">Lesbian</a></li>
+                        <li><a href="../Masturbation/01.html">Mastrbution</a></li>
+                        <li><a href="../Movies.html">Movies</a></li>
+                        <li class="logOut"><a href="../index.html">Log Out</a></li>
+                    </ul>
+                </nav>
+            </section>
+        </header>
+        `
+    }
+}
+
+customElements.define('special-header2', SpecialHeader2);
+
+//--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------
+
+class Numbers extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <div class="number">
+        <div class="light">
+            <a href="01.html">1</a>
+        </div>
+        <div>
+            <a href="02.html">2</a>
+        </div>
+        <div>
+            <a href="03.html">3</a>
+        </div>
+        <div>
+            <a href="04.html">4</a>
+        </div>
+        <div>
+            <a href="05.html">5</a>
+        </div>
+        <div>
+            <a href="06.html">6</a>
+        </div>
+        <div>
+            <a href="07.html">7</a>
+        </div>
+        <div>
+            <a href="08.html">8</a>
+        </div>
+        <div>
+            <a href="09.html">9</a>
+        </div>
+        <div>
+            <a href="10.html">10</a>
+        </div>
+        <div>
+            <a href="11.html">11</a>
+        </div>
+        <div>
+            <a href="12.html">12</a>
+        </div>
+        <div>
+            <a href="13.html">13</a>
+        </div>
+        <div>
+            <a href="14.html">14</a>
+        </div>
+        <div>
+            <a href="15.html">15</a>
+        </div>
+        <div>
+            <a href="16.html">16</a>
+        </div>
+        <div>
+            <a href="17.html">17</a>
+        </div>
+        <div>
+            <a href="18.html">18</a>
+        </div>
+        <div>
+            <a href="19.html">19</a>
+        </div>
+        <div>
+            <a href="20.html">20</a>
+        </div>
+        <div>
+            <a href="21.html">21</a>
+        </div>
+        <div>
+            <a href="22.html">22</a>
+        </div>
+        <div>
+            <a href="23.html">23</a>
+        </div>
+        <div>
+            <a href="24.html">24</a>
+        </div>
+        <div>
+            <a href="25.html">25</a>
+        </div>
+        <div>
+            <a href="26.html">26</a>
+        </div>
+        <div>
+            <a href="27.html">27</a>
+        </div>
+        <div>
+            <a href="28.html">28</a>
+        </div>
+        <div>
+            <a href="29.html">29</a>
+        </div>
+        <div>
+            <a href="30.html">30</a>
+        </div>
+        <div>
+            <a href="31.html">31</a>
+        </div>
+        <div>
+            <a href="32.html">32</a>
+        </div>
+        <div>
+            <a href="33.html">33</a>
+        </div>
+        <div>
+            <a href="34.html">34</a>
+        </div>
+        <div>
+            <a href="35.html">35</a>
+        </div>
+        <div>
+            <a href="36.html">36</a>
+        </div>
+        <div>
+            <a href="37.html">37</a>
+        </div>
+        <div>
+            <a href="38.html">38</a>
+        </div>
+        <div>
+            <a href="39.html">39</a>
+        </div>
+        <div>
+            <a href="40.html">40</a>
+        </div>
+        <div>
+            <a href="41.html">41</a>
+        </div>
+        <div>
+            <a href="42.html">42</a>
+        </div>
+        <div>
+            <a href="43.html">43</a>
+        </div>
+        <div>
+            <a href="44.html">44</a>
+        </div>
+        <div>
+            <a href="45.html">45</a>
+        </div>
+        <div>
+            <a href="46.html">46</a>
+        </div>
+        <div>
+            <a href="47.html">47</a>
+        </div>
+    </div>
+        `
+    }
+}
+
+customElements.define('numbers-list', Numbers);
+
+
+// Audio
+const audio = new Audio();
+audio.src = "../SFX/111.mp3";
+audio.play();
+// ------------------------------------------------------------------------------
+// SideBar
+
+const pathName = window.location.pathname;
+const fileName = pathName.substring(pathName.lastIndexOf('/') + 1);
+const elementsWithAttribute = document.querySelectorAll('[href]');
+elementsWithAttribute.forEach((element) => {
+    if(element.getAttribute("href") == fileName) {
+        element.parentElement.style.background = "rgb(253, 152, 30)";
+        element.parentElement.style.filter = "drop-shadow(5px 5px 5px black)";
+    }
+});
+
+let playing = document.querySelector(".playMe");
+let sideBar = document.querySelector(".sidebar");
+let close = document.querySelector(".close_btn");
+
+function OpenMenu() {
+    close.style.opacity = 1;
+    sideBar.style.left = 0;
+    playing.style.display = "none";
+}
+function CloseMenu() {
+    close.style.opacity = 0;
+    sideBar.style.left = "-75%";
+    playing.style.display = "block";
+}
+// ------------------------------------------------------------------------------
+// Up Button
+let btn = document.querySelector(".btn");
+window.onscroll = function () {
+    if (window.scrollY >= 600){
+        btn.style.display = "block";
+    }
+    else{
+        btn.style.display = "none";
+    }
+}
+btn.onclick = function() {
+    window.scrollTo({
+        left: 0,
+        top: 0,
+        behavior: "smooth"
+    });
+}
+// ------------------------------------------------------------------------------
+// Creative
+let clicky01 = document.querySelector(".clicky01");
+const audio01 = new Audio();
+audio01.src = "../SFX/kiss.mp3";
+clicky01.onclick = function() {
+    audio01.play();
+}
+let clicky02 = document.querySelector(".clicky02");
+const audio02 = new Audio();
+audio02.src = "../SFX/moan-female.mp3";
+clicky02.onclick = function() {
+    audio02.play();
+}
+let clicky03 = document.querySelector(".clicky03");
+const audio03 = new Audio();
+audio03.src = "../SFX/orgasm2.mp3";
+clicky03.onclick = function() {
+    audio03.play();
+}
+
+document.querySelector(".closyyy").onclick = function () {
+    document.querySelector(".playWithMe").style.display = "none";
+}
+function PlayWithMe () {
+    document.querySelector(".playWithMe").style.display = "flex";
+}
+// ------------------------------------------------------------------------------
+
+document.querySelector(".c_tab").addEventListener('click', display_First);
+document.querySelector(".l_tab").addEventListener('click', display_Second);
+document.querySelector(".m_tab").addEventListener('click', display_Third);
+document.querySelector(".k_tab").addEventListener('click', display_Fourth);
+
+function display_First() {
+    document.querySelector(".c_tab").classList.toggle('TAB');
+    document.querySelector(".first-tab").classList.toggle('clicked');
+}
+function display_Second() {
+    document.querySelector(".l_tab").classList.toggle('TAB');
+    document.querySelector(".second-tab").classList.toggle('clicked');
+}
+function display_Third() {
+    document.querySelector(".m_tab").classList.toggle('TAB');
+    document.querySelector(".third-tab").classList.toggle('clicked');
+}
+function display_Fourth() {
+    document.querySelector(".k_tab").classList.toggle('TAB');
+    document.querySelector(".fourth-tab").classList.toggle('clicked');
+}
+
+
+
+
+
